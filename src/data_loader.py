c0974b4cf473bda3e45b84273b03b9369fd9b238
THien2304 <hoangthienvtkn@gmail.com>
2025-12-23 22:19:21 +0700

    changes

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,10 @@
 import os
-import torch
-from torchvision import transforms
-from torch.utils.data import DataLoader, Dataset
 from PIL import Image
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 
+# ---------------- Constants ----------------
 NORM_MEAN = [0.485, 0.456, 0.406]
 NORM_STD = [0.229, 0.224, 0.225]
 IMG_SIZE = 224
@@ -15,6 +16,7 @@ def get_transforms(is_train=True):
             transforms.Resize([256, 256]),
             transforms.RandomCrop(IMG_SIZE),
             transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(NORM_MEAN, NORM_STD)
         ])
@@ -25,7 +27,7 @@ def get_transforms(is_train=True):
             transforms.Normalize(NORM_MEAN, NORM_STD)
         ])
 
-# ---------------- Custom Dataset with preloading ----------------
+# ---------------- Custom Dataset ----------------
 class CachedImageFolder(Dataset):
     def __init__(self, root_dir, transform=None):
         self.samples = []
@@ -35,14 +37,16 @@ class CachedImageFolder(Dataset):
         classes = sorted(os.listdir(root_dir))
         self.class_to_idx = {cls_name: idx for idx, cls_name in enumerate(classes)}
 
-        # preload all images into memory
+        # preload all images from subfolders
         for cls_name in classes:
             cls_path = os.path.join(root_dir, cls_name)
-            for fname in os.listdir(cls_path):
-                img_path = os.path.join(cls_path, fname)
-                img = Image.open(img_path).convert('RGB')
-                self.samples.append(img)
-                self.labels.append(self.class_to_idx[cls_name])
+            for root, _, files in os.walk(cls_path):  
+                for fname in files:
+                    if fname.lower().endswith(('.png', '.jpg', '.jpeg')):
+                        img_path = os.path.join(root, fname)
+                        img = Image.open(img_path).convert('RGB')
+                        self.samples.append(img)
+                        self.labels.append(self.class_to_idx[cls_name])
 
     def __len__(self):
         return len(self.samples)
@@ -63,9 +67,9 @@ def load_training(root_path, phase='train', batch_size=32, num_workers=4):
         dataset,
         batch_size=batch_size,
         shuffle=True,
+        drop_last=True,
         num_workers=num_workers,
-        pin_memory=True,
-        drop_last=True
+        pin_memory=True
     )
     return loader
 
@@ -83,3 +87,4 @@ def load_testing(root_path, phase='val', batch_size=32, num_workers=4):
     filenames = [f"{cls}_{i}" for i, cls in enumerate(dataset.labels)]
     labels = dataset.labels
     return loader, filenames, labels
+
