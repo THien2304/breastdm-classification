d487ab0d83edafcf08031906ddcfc364b489dacd
THien2304 <hoangthienvtkn@gmail.com>
2025-12-29 16:07:45 +0700

    aaa

--- src/data_loader.py
+++ src/data_loader.py
@@ -27,64 +27,59 @@ def get_transforms(is_train=True):
             transforms.Normalize(NORM_MEAN, NORM_STD)
         ])
 
-# ---------------- Custom Dataset ----------------
-class CachedImageFolder(Dataset):
+# ---------------- Case-Level Dataset (MIL) ----------------
+class CaseDataset(Dataset):
+    """
+    Mỗi item = tất cả ảnh của 1 case + label case
+    """
     def __init__(self, root_dir, transform=None):
-        self.samples = []
-        self.labels = []
+        self.cases = []  # list of tensors [num_imgs, C, H, W]
+        self.labels = []  # case-level label
         self.transform = transform
 
-        classes = sorted(os.listdir(root_dir))
-        self.class_to_idx = {cls_name: idx for idx, cls_name in enumerate(classes)}
+        case_dirs = sorted(os.listdir(root_dir))
+        for case_name in case_dirs:
+            case_path = os.path.join(root_dir, case_name)
+            if not os.path.isdir(case_path):
+                continue
 
-        # preload all images from subfolders
-        for cls_name in classes:
-            cls_path = os.path.join(root_dir, cls_name)
-            for root, _, files in os.walk(cls_path):  
-                for fname in files:
-                    if fname.lower().endswith(('.png', '.jpg', '.jpeg')):
-                        img_path = os.path.join(root, fname)
-                        img = Image.open(img_path).convert('RGB')
-                        self.samples.append(img)
-                        self.labels.append(self.class_to_idx[cls_name])
+            imgs = []
+            for fname in sorted(os.listdir(case_path)):
+                if fname.lower().endswith(('.png', '.jpg', '.jpeg')):
+                    img_path = os.path.join(case_path, fname)
+                    img = Image.open(img_path).convert('RGB')
+                    if self.transform:
+                        img = self.transform(img)
+                    imgs.append(img)
+            if len(imgs) > 0:
+                self.cases.append(torch.stack(imgs))  # [num_imgs, C, H, W]
+
+                # Gán label từ tên folder (ví dụ: malignant=1, benign=0)
+                if "malignant" in case_name.lower():
+                    self.labels.append(1)
+                else:
+                    self.labels.append(0)
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.cases)
 
     def __getitem__(self, idx):
-        img = self.samples[idx]
-        label = self.labels[idx]
-        if self.transform:
-            img = self.transform(img)
-        return img, label
+        return self.cases[idx], self.labels[idx]
 
-# ---------------- Loaders ----------------
-def load_training(root_path, phase='train', batch_size=32, num_workers=4):
+# ---------------- DataLoader MIL-ready ----------------
+def load_training(root_path, phase='train', batch_size=1, num_workers=2):
     data_dir = os.path.join(root_path, phase)
     transform = get_transforms(is_train=True)
-    dataset = CachedImageFolder(data_dir, transform=transform)
-    loader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        drop_last=True,
-        num_workers=num_workers,
-        pin_memory=True
-    )
+    dataset = CaseDataset(data_dir, transform=transform)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True,
+                        num_workers=num_workers, pin_memory=True)
     return loader
 
-def load_testing(root_path, phase='val', batch_size=32, num_workers=4):
+def load_testing(root_path, phase='val', batch_size=1, num_workers=2):
     data_dir = os.path.join(root_path, phase)
     transform = get_transforms(is_train=False)
-    dataset = CachedImageFolder(data_dir, transform=transform)
-    loader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=num_workers,
-        pin_memory=True
-    )
-    filenames = [f"{cls}_{i}" for i, cls in enumerate(dataset.labels)]
+    dataset = CaseDataset(data_dir, transform=transform)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=False,
+                        num_workers=num_workers, pin_memory=True)
     labels = dataset.labels
-    return loader, filenames, labels
-
+    return loader, labels
